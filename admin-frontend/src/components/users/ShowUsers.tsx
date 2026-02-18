959a97e262b37edd8e42efd539248a2ad29cd0c3
klingaje <149473210+klingaje@users.noreply.github.com>
2026-02-18 14:23:03 +0200

    users tailwind

--- admin-frontend/src/components/users/ShowUsers.tsx
+++ admin-frontend/src/components/users/ShowUsers.tsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 
 const ShowUsers = () => {
 
-    
-        const [search, setSearch] = useState("");
-    
+
+    const [search, setSearch] = useState("");
+
 
     const users = [
         { id: 1, name: "Alice", email: "Alice.Schmalice@gmail.com", active: "12.01.2026" },
@@ -23,35 +23,31 @@ const ShowUsers = () => {
 
 
             {/* Search Bar */}
-            <input 
-                type="text" 
+            <input
+                type="text"
                 placeholder="Search by name or email"
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
-                className="w-full mb-4 px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
+                className="w-67 flex justify-start mb-4 px-3 py-2 border bg-white rounded-full shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
 
-            <table className="w-full text-left">
-                <thead>
-                    <tr className="border-b">
-                        <th className="pb-2">ID</th>
-                        <th>Namn</th>
-                        <th>Email</th>
-                        <th>Senast Aktiv</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {filteredUsers.map((user) => (
-                        <tr key={user.id} className="border-b hover:bg-gray-50">
-                            <td className="py-2">{user.id}</td>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
-                            <td>{user.active}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <div className="grid grid-cols-[140px_1.6fr_2fr_140px] text-left mb-2 px-4 text-sm font-semibold text-gray-600">
+                <div className="pb-2">ID</div>
+                <div>Namn</div>
+                <div>Email</div>
+                <div>Senast Aktiv</div>
+            </div>
 
+            <div className="space-y-3">
+                {filteredUsers.map((user) => (
+                    <div key={user.id} className=/*"border-b hover:bg-gray-50"*/ "grid grid-cols-[80px_1fr_2fr_140px] bg-white rounded-xl shadow-sm hover:shadow-md hover:-translate-y-0.5 transition p-4 items-center">
+                        <div className="w-12 font-medium">{user.id}</div>
+                        <div className="w-40">{user.name}</div>
+                        <div className="flex-1">{user.email}</div>
+                        <div className="w-32 text-gray-500 text-sm">{user.active}</div>
+                    </div>
+                ))}
+            </div>
         </div >
     )
 
